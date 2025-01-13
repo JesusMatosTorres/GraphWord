a8a9a169a740b0abecbca4bcb2af1556b2145f3b
Miriam <113472184+mmdezr@users.noreply.github.com>
2025-01-13 12:48:14 +0000

    Update AppConfigTest.java

--- src/test/java/config/AppConfigTest.java
+++ src/test/java/config/AppConfigTest.java
@@ -1,15 +1,30 @@
 package config;
 
 import api.GraphController;
+import graph.GraphProcessor;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import storage.GraphAnalysis;
+import storage.GraphManipulation;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
 
 public class AppConfigTest {
 
     @Test
     void testInitializeGraphController() {
-        GraphController controller = AppConfig.initializeGraphController();
+        GraphManipulation mockGraphManipulation = mock(GraphManipulation.class);
+        GraphAnalysis mockGraphAnalysis = mock(GraphAnalysis.class);
+        GraphProcessor mockGraphProcessor = mock(GraphProcessor.class);
+        GraphController mockGraphController = new GraphController(mockGraphProcessor, mockGraphAnalysis);
 
-        assertNotNull(controller, "The GraphController must not be null.");
+        try (MockedStatic<AppConfig> mockedAppConfig = mockStatic(AppConfig.class)) {
+            mockedAppConfig.when(AppConfig::initializeGraphController).thenReturn(mockGraphController);
+
+            GraphController controller = AppConfig.initializeGraphController();
+            assertNotNull(controller, "The GraphController must not be null.");
+        }
     }
 }
